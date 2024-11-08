23051df8fe6b122f49346750e44a2ce47fee5474
Jason Hancock <jason@jasonhancock.com>
2024-11-07 22:44:14 -0800

    add float32/64, make check function generic for numeric types

--- flags/int.go
+++ flags/int.go
@@ -8,17 +8,17 @@ import (
 )
 
 func init() {
-	registerType("*int", intAdd, intCheck)
-	registerType("*int8", int8Add, int8Check)
-	registerType("*int16", int16Add, int16Check)
-	registerType("*int32", int32Add, int32Check)
-	registerType("*int64", int64Add, int64Check)
+	registerType("*int", intAdd, numberCheck[int])
+	registerType("*int8", int8Add, numberCheck[int8])
+	registerType("*int16", int16Add, numberCheck[int16])
+	registerType("*int32", int32Add, numberCheck[int32])
+	registerType("*int64", int64Add, numberCheck[int64])
 
-	registerType("*uint", uintAdd, uintCheck)
-	registerType("*uint8", uint8Add, uint8Check)
-	registerType("*uint16", uint16Add, uint16Check)
-	registerType("*uint32", uint32Add, uint32Check)
-	registerType("*uint64", uint64Add, uint64Check)
+	registerType("*uint", uintAdd, numberCheck[uint])
+	registerType("*uint8", uint8Add, numberCheck[uint8])
+	registerType("*uint16", uint16Add, numberCheck[uint16])
+	registerType("*uint32", uint32Add, numberCheck[uint32])
+	registerType("*uint64", uint64Add, numberCheck[uint64])
 }
 
 func intAdd(fs *pflag.FlagSet, f *flag) {
@@ -220,118 +220,4 @@ func uint64Add(fs *pflag.FlagSet, f *flag) {
 		}
 	}
 	fs.Uint64Var(f.dest.(*uint64), f.name, defaultVal, f.Usage())
-}
-
-func intCheck(f *flag) error {
-	val, ok := f.dest.(*int)
-	if !ok {
-		return fmt.Errorf("%q not an int", f.name)
-	}
-	if *val == 0 {
-		return fmt.Errorf("required value %q not specified", f.name)
-	}
-	return nil
-}
-
-func int8Check(f *flag) error {
-	val, ok := f.dest.(*int8)
-	if !ok {
-		return fmt.Errorf("%q not an int8", f.name)
-	}
-	if *val == 0 {
-		return fmt.Errorf("required value %q not specified", f.name)
-	}
-
-	return nil
-}
-
-func int16Check(f *flag) error {
-	val, ok := f.dest.(*int16)
-	if !ok {
-		return fmt.Errorf("%q not an int16", f.name)
-	}
-	if *val == 0 {
-		return fmt.Errorf("required value %q not specified", f.name)
-	}
-
-	return nil
-}
-
-func int32Check(f *flag) error {
-	val, ok := f.dest.(*int32)
-	if !ok {
-		return fmt.Errorf("%q not an int32", f.name)
-	}
-	if *val == 0 {
-		return fmt.Errorf("required value %q not specified", f.name)
-	}
-
-	return nil
-}
-
-func int64Check(f *flag) error {
-	val, ok := f.dest.(*int64)
-	if !ok {
-		return fmt.Errorf("%q not an int64", f.name)
-	}
-	if *val == 0 {
-		return fmt.Errorf("required value %q not specified", f.name)
-	}
-
-	return nil
-}
-
-func uintCheck(f *flag) error {
-	val, ok := f.dest.(*uint)
-	if !ok {
-		return fmt.Errorf("%q not an uint", f.name)
-	}
-	if *val == 0 {
-		return fmt.Errorf("required value %q not specified", f.name)
-	}
-	return nil
-}
-
-func uint8Check(f *flag) error {
-	val, ok := f.dest.(*uint8)
-	if !ok {
-		return fmt.Errorf("%q not an uint8", f.name)
-	}
-	if *val == 0 {
-		return fmt.Errorf("required value %q not specified", f.name)
-	}
-	return nil
-}
-
-func uint16Check(f *flag) error {
-	val, ok := f.dest.(*uint16)
-	if !ok {
-		return fmt.Errorf("%q not an uint16", f.name)
-	}
-	if *val == 0 {
-		return fmt.Errorf("required value %q not specified", f.name)
-	}
-	return nil
-}
-
-func uint32Check(f *flag) error {
-	val, ok := f.dest.(*uint32)
-	if !ok {
-		return fmt.Errorf("%q not an uint32", f.name)
-	}
-	if *val == 0 {
-		return fmt.Errorf("required value %q not specified", f.name)
-	}
-	return nil
-}
-
-func uint64Check(f *flag) error {
-	val, ok := f.dest.(*uint64)
-	if !ok {
-		return fmt.Errorf("%q not an uint64", f.name)
-	}
-	if *val == 0 {
-		return fmt.Errorf("required value %q not specified", f.name)
-	}
-	return nil
 }
